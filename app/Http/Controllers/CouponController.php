fadadfceae743c12e7de0a5392a4faf7be40728b
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-21 10:21:02 +0100

    added CouponController

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -14,72 +14,61 @@ class CouponController extends Controller
      */
     public function index()
     {
-        //
+        $result['data']=Coupon::all();
+        return view('admin/coupon',$result);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    
+    public function manage_coupon(Request $request,$id='')
     {
-        //
+        if($id>0){
+            $arr=Coupon::where(['id'=>$id])->get(); 
+
+            $result['title']=$arr['0']->title;
+            $result['code']=$arr['0']->code;
+            $result['value']=$arr['0']->value;
+            $result['id']=$arr['0']->id;
+        }else{
+            $result['title']='';
+            $result['code']='';
+            $result['value']='';
+            $result['id']=0;
+            
+        }
+        return view('admin/manage_coupon',$result);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function manage_coupon_process(Request $request)
     {
-        //
+        //return $request->post();
+        
+        $request->validate([
+            'title'=>'required',
+            'code'=>'required|unique:coupons,code,'.$request->post('id'),   
+            'value'=>'required',
+        ]);
+
+        if($request->post('id')>0){
+            $model=Coupon::find($request->post('id'));
+            $msg="Coupon updated";
+        }else{
+            $model=new Coupon();
+            $msg="Coupon inserted";
+        }
+        $model->title=$request->post('title');
+        $model->code=$request->post('code');
+        $model->value=$request->post('value');
+        $model->save();
+        $request->session()->flash('message',$msg);
+        return redirect('admin/coupon');
+        
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Coupon  $coupon
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Coupon $coupon)
-    {
-        //
+    public function delete(Request $request,$id){
+        $model=Coupon::find($id);
+        $model->delete();
+        $request->session()->flash('message','Coupon deleted');
+        return redirect('admin/coupon');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Coupon  $coupon
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Coupon $coupon)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Coupon  $coupon
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Coupon $coupon)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Coupon  $coupon
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Coupon $coupon)
-    {
-        //
-    }
 }
