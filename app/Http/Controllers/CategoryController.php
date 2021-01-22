99e7354f0b0a2a55b9cb74d7eb7878dff6989c40
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-22 08:36:48 +0100

    updated CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -48,6 +48,7 @@ class CategoryController extends Controller
         }
         $model->category_name = $request->post('category_name');
         $model->category_slug = $request->post('category_slug');
+        $model->status = 1;
         $model->save();
         $request->session()->flash('message', $msg);
         return redirect('admin/category');
@@ -58,6 +59,15 @@ class CategoryController extends Controller
         $model = Category::find($id);
         $model->delete();
         $request->session()->flash('message', 'Category deleted');
+        return redirect('admin/category');
+    }
+
+    public function status(Request $request, $status, $id)
+    {
+        $model = Category::find($id);
+        $model->status = $status;
+        $model->save();
+        $request->session()->flash('message', 'Category status updated');
         return redirect('admin/category');
     }
 }
