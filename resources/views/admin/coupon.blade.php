6055d19406c7b2a9b7644d4656e167cca74230bc
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-22 08:31:10 +0100

    updated admin active pages options

--- resources/views/admin/coupon.blade.php
+++ resources/views/admin/coupon.blade.php
@@ -1,10 +1,10 @@
 @extends('admin/layout')
-@section('page_title','Coupon')
-@section('coupon_select','active')
+@section('page_title', 'Coupon')
+@section('coupon_select', 'active')
 @section('container')
-    {{session('message')}}                          
+    {{ session('message') }}
     <h1 class="mb10">Coupon</h1>
-    <a href="{{url('admin/coupon/manage_coupon')}}">
+    <a href="{{ url('admin/coupon/manage_coupon') }}">
         <button type="button" class="btn btn-success">
             Add Coupon
         </button>
@@ -24,25 +24,29 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($data as $list)
-                        <tr>
-                            <td>{{$list->id}}</td>
-                            <td>{{$list->title}}</td>
-                            <td>{{$list->code}}</td>
-                            <td>{{$list->value}}</td>
-                            <td>
-                                <a href="{{url('admin/coupon/manage_coupon/')}}/{{$list->id}}"><button type="button" class="btn btn-success">Edit</button></a>
+                        @foreach ($data as $list)
+                            <tr>
+                                <td>{{ $list->id }}</td>
+                                <td>{{ $list->title }}</td>
+                                <td>{{ $list->code }}</td>
+                                <td>{{ $list->value }}</td>
+                                <td>
+                                    <a href="{{ url('admin/coupon/manage_coupon/') }}/{{ $list->id }}"><button type="button"
+                                            class="btn btn-success">Edit</button></a>
 
-                                @if($list->status==1)
-                                    <a href="{{url('admin/coupon/status/0')}}/{{$list->id}}"><button type="button" class="btn btn-primary">Active</button></a>
-                                 @elseif($list->status==0)
-                                    <a href="{{url('admin/coupon/status/1')}}/{{$list->id}}"><button type="button" class="btn btn-warning">Deactive</button></a>
-                                @endif
-                                
-                                <a href="{{url('admin/coupon/delete/')}}/{{$list->id}}"><button type="button" class="btn btn-danger">Delete</button></a>
-                                
-                            </td>
-                        </tr>
+                                    @if ($list->status == 1)
+                                        <a href="{{ url('admin/coupon/status/0') }}/{{ $list->id }}"><button type="button"
+                                                class="btn btn-primary">Active</button></a>
+                                    @elseif($list->status==0)
+                                        <a href="{{ url('admin/coupon/status/1') }}/{{ $list->id }}"><button type="button"
+                                                class="btn btn-warning">Deactive</button></a>
+                                    @endif
+
+                                    <a href="{{ url('admin/coupon/delete/') }}/{{ $list->id }}"><button type="button"
+                                            class="btn btn-danger">Delete</button></a>
+
+                                </td>
+                            </tr>
                         @endforeach
                     </tbody>
                 </table>
@@ -50,5 +54,5 @@
             <!-- END DATA TABLE-->
         </div>
     </div>
-                        
+
 @endsection
