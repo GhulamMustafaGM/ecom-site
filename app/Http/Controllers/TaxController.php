adfc0430422d617d44666932c42a2a8a8be84bbc
Ghulam Mustafa <mustafaji@gmail.com>
2021-02-06 22:53:16 +0100

    added TaxController

--- app/Http/Controllers/TaxController.php
+++ app/Http/Controllers/TaxController.php
@@ -7,79 +7,68 @@ use Illuminate\Http\Request;
 
 class TaxController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $result['data'] = Tax::all();
+        return view('admin/tax', $result);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+
+    public function manage_tax(Request $request, $id = '')
     {
-        //
+        if ($id > 0) {
+            $arr = Tax::where(['id' => $id])->get();
+
+            $result['tax_desc'] = $arr['0']->tax_desc;
+            $result['tax_value'] = $arr['0']->tax_value;
+            $result['status'] = $arr['0']->status;
+            $result['id'] = $arr['0']->id;
+        } else {
+            $result['tax_desc'] = '';
+            $result['tax_value'] = '';
+            $result['status'] = '';
+            $result['id'] = 0;
+        }
+        return view('admin/manage_tax', $result);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function manage_tax_process(Request $request)
     {
-        //
+        //return $request->post();
+
+        $request->validate([
+            'tax_value' => 'required|unique:taxs,tax_value,' . $request->post('id'),
+        ]);
+
+        if ($request->post('id') > 0) {
+            $model = Tax::find($request->post('id'));
+            $msg = "Tax updated";
+        } else {
+            $model = new Tax();
+            $msg = "Tax inserted";
+        }
+        $model->tax_desc = $request->post('tax_desc');
+        $model->tax_value = $request->post('tax_value');
+        $model->status = 1;
+        $model->save();
+        $request->session()->flash('message', $msg);
+        return redirect('admin/tax');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Tax  $tax
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Tax $tax)
+    public function delete(Request $request, $id)
     {
-        //
+        $model = Tax::find($id);
+        $model->delete();
+        $request->session()->flash('message', 'Tax deleted');
+        return redirect('admin/tax');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Tax  $tax
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Tax $tax)
+    public function status(Request $request, $status, $id)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Tax  $tax
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Tax $tax)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Tax  $tax
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Tax $tax)
-    {
-        //
+        $model = Tax::find($id);
+        $model->status = $status;
+        $model->save();
+        $request->session()->flash('message', 'Tax status updated');
+        return redirect('admin/tax');
     }
 }
