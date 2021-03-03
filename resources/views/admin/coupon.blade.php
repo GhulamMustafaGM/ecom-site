98e687c5a961c9a20b59c107a28a99e503215d9e
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-03 23:18:09 +0100

    updated admin coupon view

--- resources/views/admin/coupon.blade.php
+++ resources/views/admin/coupon.blade.php
@@ -38,17 +38,20 @@
                                 <td>{{ $list->code }}</td>
                                 <td>{{ $list->value }}</td>
                                 <td>
-                                    <a href="{{ url('admin/coupon/manage_coupon/') }}/{{ $list->id }}"><button type="button"
-                                            class="btn btn-success">Edit</button></a>
+                                    <a href="{{ url('admin/coupon/manage_coupon/') }}/{{ $list->id }}"><button
+                                            type="button" class="btn btn-success">Edit</button></a>
+
                                     @if ($list->status == 1)
-                                        <a href="{{ url('admin/coupon/status/0') }}/{{ $list->id }}"><button type="button"
-                                                class="btn btn-primary">Active</button></a>
+                                        <a href="{{ url('admin/coupon/status/0') }}/{{ $list->id }}"><button
+                                                type="button" class="btn btn-primary">Active</button></a>
                                     @elseif($list->status==0)
-                                        <a href="{{ url('admin/coupon/status/1') }}/{{ $list->id }}"><button type="button"
-                                                class="btn btn-warning">Deactive</button></a>
+                                        <a href="{{ url('admin/coupon/status/1') }}/{{ $list->id }}"><button
+                                                type="button" class="btn btn-warning">Deactive</button></a>
                                     @endif
+
                                     <a href="{{ url('admin/coupon/delete/') }}/{{ $list->id }}"><button type="button"
                                             class="btn btn-danger">Delete</button></a>
+
                                 </td>
                             </tr>
                         @endforeach
