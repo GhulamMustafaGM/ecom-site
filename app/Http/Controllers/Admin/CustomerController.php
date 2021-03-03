58cd3ee472c7e02f34f69a2f5a9ab76a24ea856e
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-03 23:40:44 +0100

    updated CustomerController

--- app/Http/Controllers/Admin/CustomerController.php
+++ app/Http/Controllers/Admin/CustomerController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-use App\Http\Controllers\Controller;
 
+use App\Http\Controllers\Controller;
 use App\Models\Admin\Customer;
 use Illuminate\Http\Request;
 
@@ -10,23 +10,23 @@ class CustomerController extends Controller
 {
     public function index()
     {
-        $result['data']=Customer::all();
-        return view('admin/customer',$result);
+        $result['data'] = Customer::all();
+        return view('admin/customer', $result);
     }
 
-    
-    public function show(Request $request,$id='')
+    public function show(Request $request, $id = '')
     {
-        $arr=Customer::where(['id'=>$id])->get(); 
-        $result['customer_list']=$arr['0'];
-        return view('admin/show_customer',$result);
+        $arr = Customer::where(['id' => $id])->get();
+        $result['customer_list'] = $arr['0'];
+        return view('admin/show_customer', $result);
     }
 
-    public function status(Request $request,$status,$id){
-        $model=Customer::find($id);
-        $model->status=$status;
+    public function status(Request $request, $status, $id)
+    {
+        $model = Customer::find($id);
+        $model->status = $status;
         $model->save();
-        $request->session()->flash('message','Customer status updated');
+        $request->session()->flash('message', 'Customer status updated');
         return redirect('admin/customer');
     }
 }
