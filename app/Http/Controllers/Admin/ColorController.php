6a04dbbcd38a8bd4389e5d9f5e059b162c4bb35c
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-03 23:39:22 +0100

    updated ColorController

--- app/Http/Controllers/Admin/ColorController.php
+++ app/Http/Controllers/Admin/ColorController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-use App\Http\Controllers\Controller;
 
+use App\Http\Controllers\Controller;
 use App\Models\Admin\Color;
 use Illuminate\Http\Request;
 
@@ -10,63 +10,64 @@ class ColorController extends Controller
 {
     public function index()
     {
-        $result['data']=Color::all();
-        return view('admin/color',$result);
+        $result['data'] = Color::all();
+        return view('admin/color', $result);
     }
 
-    
-    public function manage_color(Request $request,$id='')
+    public function manage_color(Request $request, $id = '')
     {
-        if($id>0){
-            $arr=Color::where(['id'=>$id])->get(); 
+        if ($id > 0) {
+            $arr = Color::where(['id' => $id])->get();
 
-            $result['color']=$arr['0']->color;
-            $result['status']=$arr['0']->status;
-            $result['id']=$arr['0']->id;
-        }else{
-            $result['color']='';
-            $result['status']='';
-            $result['id']=0;
-            
+            $result['color'] = $arr['0']->color;
+            $result['status'] = $arr['0']->status;
+            $result['id'] = $arr['0']->id;
+        } else {
+            $result['color'] = '';
+            $result['status'] = '';
+            $result['id'] = 0;
+
         }
-        return view('admin/manage_color',$result);
+        return view('admin/manage_color', $result);
     }
 
     public function manage_color_process(Request $request)
     {
         //return $request->post();
-        
+
         $request->validate([
-            'color'=>'required|unique:colors,color,'.$request->post('id'),   
+            'color' => 'required|unique:colors,color,' . $request->post('id'),
         ]);
 
-        if($request->post('id')>0){
-            $model=Color::find($request->post('id'));
-            $msg="Color updated";
-        }else{
-            $model=new Color();
-            $msg="Color inserted";
+        if ($request->post('id') > 0) {
+            $model = Color::find($request->post('id'));
+            $msg = "Color updated";
+        } else {
+            $model = new Color();
+            $msg = "Color inserted";
         }
-        $model->color=$request->post('color');
-        $model->status=1;
+        $model->color = $request->post('color');
+        $model->status = 1;
         $model->save();
-        $request->session()->flash('message',$msg);
+        $request->session()->flash('message', $msg);
         return redirect('admin/color');
-        
+
     }
 
-    public function delete(Request $request,$id){
-        $model=color::find($id);
+    public function delete(Request $request, $id)
+    {
+        $model = color::find($id);
         $model->delete();
-        $request->session()->flash('message','Color deleted');
+        $request->session()->flash('message', 'Color deleted');
         return redirect('admin/color');
     }
 
-    public function status(Request $request,$status,$id){
-        $model=Color::find($id);
-        $model->status=$status;
+    public function status(Request $request, $status, $id)
+    {
+        $model = Color::find($id);
+        $model->status = $status;
         $model->save();
-        $request->session()->flash('message','Color status updated');
+        $request->session()->flash('message', 'Color status updated');
         return redirect('admin/color');
     }
 }
