e34ac4dfcab17ffba302ad2dc3d16791e1ec91d6
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-03 23:35:39 +0100

    updated AdminController

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -16,9 +16,9 @@ class AdminController extends Controller
      */
     public function index(Request $request)
     {
-        if($request->session()->has('ADMIN_LOGIN')){
+        if ($request->session()->has('ADMIN_LOGIN')) {
             return redirect('admin/dashboard');
-        }else{
+        } else {
             return view('admin.login');
         }
         return view('admin.login');
@@ -26,22 +26,22 @@ class AdminController extends Controller
 
     public function auth(Request $request)
     {
-        $email=$request->post('email');
-        $password=$request->post('password');
+        $email = $request->post('email');
+        $password = $request->post('password');
 
         // $result=Admin::where(['email'=>$email,'password'=>$password])->get();
-        $result=Admin::where(['email'=>$email])->first();
-        if($result){
-            if(Hash::check($request->post('password'),$result->password)){
-                $request->session()->put('ADMIN_LOGIN',true);
-                $request->session()->put('ADMIN_ID',$result->id);
+        $result = Admin::where(['email' => $email])->first();
+        if ($result) {
+            if (Hash::check($request->post('password'), $result->password)) {
+                $request->session()->put('ADMIN_LOGIN', true);
+                $request->session()->put('ADMIN_ID', $result->id);
                 return redirect('admin/dashboard');
-            }else{
-                $request->session()->flash('error','Please enter correct password');
+            } else {
+                $request->session()->flash('error', 'Please enter correct password');
                 return redirect('admin');
             }
-        }else{
-            $request->session()->flash('error','Please enter valid login details');
+        } else {
+            $request->session()->flash('error', 'Please enter valid login details');
             return redirect('admin');
         }
     }
