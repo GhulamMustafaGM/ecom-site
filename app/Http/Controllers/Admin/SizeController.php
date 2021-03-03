06ce2ce06c7e06dd541ab9bf2cc084a640ec9715
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-03 23:45:24 +0100

    updated SizeControler and TaxController

--- app/Http/Controllers/Admin/SizeController.php
+++ app/Http/Controllers/Admin/SizeController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-use App\Http\Controllers\Controller;
 
+use App\Http\Controllers\Controller;
 use App\Models\Admin\Size;
 use Illuminate\Http\Request;
 
@@ -10,63 +10,64 @@ class SizeController extends Controller
 {
     public function index()
     {
-        $result['data']=Size::all();
-        return view('admin/size',$result);
+        $result['data'] = Size::all();
+        return view('admin/size', $result);
     }
 
-    
-    public function manage_size(Request $request,$id='')
+    public function manage_size(Request $request, $id = '')
     {
-        if($id>0){
-            $arr=Size::where(['id'=>$id])->get(); 
+        if ($id > 0) {
+            $arr = Size::where(['id' => $id])->get();
 
-            $result['size']=$arr['0']->size;
-            $result['status']=$arr['0']->status;
-            $result['id']=$arr['0']->id;
-        }else{
-            $result['size']='';
-            $result['status']='';
-            $result['id']=0;
-            
+            $result['size'] = $arr['0']->size;
+            $result['status'] = $arr['0']->status;
+            $result['id'] = $arr['0']->id;
+        } else {
+            $result['size'] = '';
+            $result['status'] = '';
+            $result['id'] = 0;
+
         }
-        return view('admin/manage_size',$result);
+        return view('admin/manage_size', $result);
     }
 
     public function manage_size_process(Request $request)
     {
         //return $request->post();
-        
+
         $request->validate([
-            'size'=>'required|unique:sizes,size,'.$request->post('id'),   
+            'size' => 'required|unique:sizes,size,' . $request->post('id'),
         ]);
 
-        if($request->post('id')>0){
-            $model=Size::find($request->post('id'));
-            $msg="Size updated";
-        }else{
-            $model=new Size();
-            $msg="Size inserted";
+        if ($request->post('id') > 0) {
+            $model = Size::find($request->post('id'));
+            $msg = "Size updated";
+        } else {
+            $model = new Size();
+            $msg = "Size inserted";
         }
-        $model->size=$request->post('size');
-        $model->status=1;
+        $model->size = $request->post('size');
+        $model->status = 1;
         $model->save();
-        $request->session()->flash('message',$msg);
+        $request->session()->flash('message', $msg);
         return redirect('admin/size');
-        
+
     }
 
-    public function delete(Request $request,$id){
-        $model=Size::find($id);
+    public function delete(Request $request, $id)
+    {
+        $model = Size::find($id);
         $model->delete();
-        $request->session()->flash('message','Size deleted');
+        $request->session()->flash('message', 'Size deleted');
         return redirect('admin/size');
     }
 
-    public function status(Request $request,$status,$id){
-        $model=Size::find($id);
-        $model->status=$status;
+    public function status(Request $request, $status, $id)
+    {
+        $model = Size::find($id);
+        $model->status = $status;
         $model->save();
-        $request->session()->flash('message','Size status updated');
+        $request->session()->flash('message', 'Size status updated');
         return redirect('admin/size');
     }
 }
--- app/Http/Controllers/Admin/TaxController.php
+++ app/Http/Controllers/Admin/TaxController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-use App\Http\Controllers\Controller;
 
+use App\Http\Controllers\Controller;
 use App\Models\Admin\Tax;
 use Illuminate\Http\Request;
 
@@ -10,66 +10,67 @@ class TaxController extends Controller
 {
     public function index()
     {
-        $result['data']=Tax::all();
-        return view('admin/tax',$result);
+        $result['data'] = Tax::all();
+        return view('admin/tax', $result);
     }
 
-    
-    public function manage_tax(Request $request,$id='')
+    public function manage_tax(Request $request, $id = '')
     {
-        if($id>0){
-            $arr=Tax::where(['id'=>$id])->get(); 
+        if ($id > 0) {
+            $arr = Tax::where(['id' => $id])->get();
 
-            $result['tax_desc']=$arr['0']->tax_desc;
-            $result['tax_value']=$arr['0']->tax_value;
-            $result['status']=$arr['0']->status;
-            $result['id']=$arr['0']->id;
-        }else{
-            $result['tax_desc']='';
-            $result['tax_value']='';
-            $result['status']='';
-            $result['id']=0;
-            
+            $result['tax_desc'] = $arr['0']->tax_desc;
+            $result['tax_value'] = $arr['0']->tax_value;
+            $result['status'] = $arr['0']->status;
+            $result['id'] = $arr['0']->id;
+        } else {
+            $result['tax_desc'] = '';
+            $result['tax_value'] = '';
+            $result['status'] = '';
+            $result['id'] = 0;
+
         }
-        return view('admin/manage_tax',$result);
+        return view('admin/manage_tax', $result);
     }
 
     public function manage_tax_process(Request $request)
     {
         //return $request->post();
-        
+
         $request->validate([
-            'tax_value'=>'required|unique:taxs,tax_value,'.$request->post('id'),   
+            'tax_value' => 'required|unique:taxs,tax_value,' . $request->post('id'),
         ]);
 
-        if($request->post('id')>0){
-            $model=Tax::find($request->post('id'));
-            $msg="Tax updated";
-        }else{
-            $model=new Tax();
-            $msg="Tax inserted";
+        if ($request->post('id') > 0) {
+            $model = Tax::find($request->post('id'));
+            $msg = "Tax updated";
+        } else {
+            $model = new Tax();
+            $msg = "Tax inserted";
         }
-        $model->tax_desc=$request->post('tax_desc');
-        $model->tax_value=$request->post('tax_value');
-        $model->status=1;
+        $model->tax_desc = $request->post('tax_desc');
+        $model->tax_value = $request->post('tax_value');
+        $model->status = 1;
         $model->save();
-        $request->session()->flash('message',$msg);
+        $request->session()->flash('message', $msg);
         return redirect('admin/tax');
-        
+
     }
 
-    public function delete(Request $request,$id){
-        $model=Tax::find($id);
+    public function delete(Request $request, $id)
+    {
+        $model = Tax::find($id);
         $model->delete();
-        $request->session()->flash('message','Tax deleted');
+        $request->session()->flash('message', 'Tax deleted');
         return redirect('admin/tax');
     }
 
-    public function status(Request $request,$status,$id){
-        $model=Tax::find($id);
-        $model->status=$status;
+    public function status(Request $request, $status, $id)
+    {
+        $model = Tax::find($id);
+        $model->status = $status;
         $model->save();
-        $request->session()->flash('message','Tax status updated');
+        $request->session()->flash('message', 'Tax status updated');
         return redirect('admin/tax');
     }
 }
