7690955e8067fb766d0cb0dad57d800645ced1d5
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-10 14:17:50 +0100

    updated frontend layout

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -149,8 +149,9 @@
                             <!-- search box -->
                             <div class="aa-search-box">
                                 <form action="">
-                                    <input type="text" name="" id="" placeholder="Search here ex. 'man' ">
-                                    <button type="submit"><span class="fa fa-search"></span></button>
+                                    <input type="text" id="search_str" placeholder="Search here ex. 'man' ">
+                                    <button type="button" onclick="funSearch()"><span
+                                            class="fa fa-search"></span></button>
                                 </form>
                             </div>
                             <!-- / search box -->
