37df2e6f0093a1237241c244121d085263459e92
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-15 23:19:08 +0100

    updated routes web

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,13 @@ Route::get('cart', [FrontController::class, 'cart']);
 Route::get('search/{str}', [FrontController::class, 'search']);
 Route::get('registration', [FrontController::class, 'registration']);
 Route::post('registration_process', [FrontController::class, 'registration_process'])->name('registration.registration_process');
+Route::post('login_process', [FrontController::class, 'login_process'])->name('login.login_process');
+Route::get('logout', function () {
+    session()->forget('FRONT_USER_LOGIN');
+    session()->forget('FRONT_USER_ID');
+    session()->forget('FRONT_USER_NAME');
+    return redirect('/');
+});
 
 Route::get('admin', [AdminController::class, 'index']);
 Route::post('admin/auth', [AdminController::class, 'auth'])->name('admin.auth');
@@ -110,7 +117,7 @@ Route::group(['middleware' => 'admin_auth'], function () {
     Route::get('admin/logout', function () {
         session()->forget('ADMIN_LOGIN');
         session()->forget('ADMIN_ID');
-        session()->flash('error', 'Logout successfully');
+        session()->flash('error', 'Logout sucessfully');
         return redirect('admin');
     });
 });
